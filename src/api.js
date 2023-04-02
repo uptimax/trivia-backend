a8480526abfad90e9dd6c5918e69cda9430d6800
SpendnWin <129542590+SpendnWin@users.noreply.github.com>
2023-04-02 23:36:28 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const serverless = require('serverless-http');
+const serverless = require('serverless-http');
 const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require('cors');
