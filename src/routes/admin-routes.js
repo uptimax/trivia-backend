122c24a82faaf1830667a2153260fd678ca1bcdf
SpendnWin <129542590+SpendnWin@users.noreply.github.com>
2023-04-03 01:11:06 +0100

    Update admin-routes.js

--- src/routes/admin-routes.js
+++ src/routes/admin-routes.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const { adminLogin, adminSignup, getUserByEmail, getAllUsers, getAllQuiz, getQuizPaticipants, redeemQuizByEmailAndToken, convertCSVtoJson, CreateGame } = require('../controllers/adminController');
 const { ConvertCsvToJson } = require('../csvtojson_converter');
 
-const uploads = multer({dest: __dirname + '/../' + 'files/csv'});
+// const uploads = multer({dest: __dirname + '/../' + 'files/csv'});
 
 const router = express.Router();
 let i;
@@ -18,28 +18,28 @@ router.post('/get_quiz_participants', getQuizPaticipants);
 router.post('/redeem_quiz_by_email_token', redeemQuizByEmailAndToken);
 router.post('/create_new_game', CreateGame);
 
-router.post('/convert_csv', uploads.single('file'), async (req, res, next)=>{
- try{
- if(includes(req, 'file')){
-    console.log('here');
-    if(!includes(req.file, 'path')){ //
-        res.status(400).send({error: {file_not_found: true}});
-        return;
-    }
- }
- console.log('super');
+// router.post('/convert_csv', uploads.single('file'), async (req, res, next)=>{
+//  try{
+//  if(includes(req, 'file')){
+//     console.log('here');
+//     if(!includes(req.file, 'path')){ //
+//         res.status(400).send({error: {file_not_found: true}});
+//         return;
+//     }
+//  }
+//  console.log('super');
  
- let json = await ConvertCsvToJson(req.file.path);
+//  let json = await ConvertCsvToJson(req.file.path);
 
- await fs.unlink(req.file.path, (err) => {});
- res.status(200).send({success: true, data: json});
-}catch(err){
-    console.log(err);
-    res.status(400).send({error: {
-        something_went_wrong: true
-    }});
-}
-});
+//  await fs.unlink(req.file.path, (err) => {});
+//  res.status(200).send({success: true, data: json});
+// }catch(err){
+//     console.log(err);
+//     res.status(400).send({error: {
+//         something_went_wrong: true
+//     }});
+// }
+// });
 
 function includes(data, key) {
     return Object.keys(data).includes(key);
